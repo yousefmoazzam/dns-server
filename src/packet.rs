cc9dff965feee9590b24835ef18895271f7582e0
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-10 00:32:38 +0000

    Add packet buffer struct and current position in buffer getter

---
+++ src/packet.rs
@@ -0,0 +1,28 @@
+const PACKET_BYTES_LENGTH: usize = 512;
+
+pub struct PacketBuffer {
+    buf: [u8; PACKET_BYTES_LENGTH],
+    pos: usize,
+}
+
+impl PacketBuffer {
+    pub fn new(buf: [u8; PACKET_BYTES_LENGTH]) -> PacketBuffer {
+        PacketBuffer { buf, pos: 0 }
+    }
+
+    pub fn pos(&self) -> usize {
+        self.pos
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn current_position_within_new_packet_buffer_is_zero() {
+        let buf = [0; PACKET_BYTES_LENGTH];
+        let packet_buffer = PacketBuffer::new(buf);
+        assert_eq!(0, packet_buffer.pos());
+    }
+}
