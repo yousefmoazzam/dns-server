5e8f5995b8b6a51012723acf53c8cbbc7b51c622
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-11-10 00:41:04 +0000

    Add method to step position forward in buffer

--- src/packet.rs
+++ src/packet.rs
@@ -13,6 +13,10 @@ impl PacketBuffer {
     pub fn pos(&self) -> usize {
         self.pos
     }
+
+    pub fn step(&mut self, step: usize) {
+        self.pos += step;
+    }
 }
 
 #[cfg(test)]
@@ -24,5 +28,14 @@ mod tests {
         let buf = [0; PACKET_BYTES_LENGTH];
         let packet_buffer = PacketBuffer::new(buf);
         assert_eq!(0, packet_buffer.pos());
+    }
+
+    #[test]
+    fn step_position_forward_in_buffer() {
+        let buf = [0; PACKET_BYTES_LENGTH];
+        let mut packet_buffer = PacketBuffer::new(buf);
+        let step = 5;
+        packet_buffer.step(step);
+        assert_eq!(step, packet_buffer.pos());
     }
 }
